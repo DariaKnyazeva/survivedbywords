ab9b1acecd9443122d4d7c1ec528fe36dd317f07
Dariak <dashaa@equinox.(none)>
2014-08-12 16:37:27 +0700

    Added publishers, authors paginated lists

--- static/js/script.js
+++ static/js/script.js
@@ -38,7 +38,6 @@ app.controller('AppController', function ($scope, $http) {
         $http.get('/booksapi?page=' + pageNumber)
             .then(function(result) {
                 $scope.books = result.data.results;
-                console.log(result.data.results);
                 $scope.totalBooks = result.data.count;
             });
     }
@@ -63,3 +62,55 @@ app.controller('BookEditController', [
     };
   }
 ]);
+
+
+app = angular.module ('survivedbywords.app.authors', ['ngResource', 'angularUtils.directives.dirPagination']);
+
+app.controller('AppController', function ($scope, $http) {
+    $scope.authors = [],
+    $scope.totalAuthors = 0,
+    $scope.numPerPage = 10;  
+    getResultsPage(1);
+
+    $scope.pagination = {
+        current: 1
+    };
+
+    $scope.pageChanged = function(newPage) {
+        getResultsPage(newPage);
+    };
+
+    function getResultsPage(pageNumber) {
+        $http.get('/authorsapi?page=' + pageNumber)
+            .then(function(result) {
+                $scope.authors = result.data.results;
+                $scope.totalAuthors = result.data.count;
+            });
+    }
+});
+
+
+app = angular.module ('survivedbywords.app.publishers', ['ngResource', 'angularUtils.directives.dirPagination']);
+
+app.controller('AppController', function ($scope, $http) {
+    $scope.publishers = [],
+    $scope.totalPublishers = 0,
+    $scope.numPerPage = 10;  
+    getResultsPage(1);
+
+    $scope.pagination = {
+        current: 1
+    };
+
+    $scope.pageChanged = function(newPage) {
+        getResultsPage(newPage);
+    };
+
+    function getResultsPage(pageNumber) {
+        $http.get('/publishersapi?page=' + pageNumber)
+            .then(function(result) {
+                $scope.publishers = result.data.results;
+                $scope.totalPublishers = result.data.count;
+            });
+    }
+});
